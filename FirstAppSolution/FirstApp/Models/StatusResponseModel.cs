aea62c29da4ab20ae4a65ff90dd9a21072eddeb8
Jeffry Gonzalez <jeff@hypertheory.com>
2023-07-19 11:24:18 -0400

    added some demos

---
+++ FirstAppSolution/FirstApp/Models/StatusResponseModel.cs
@@ -0,0 +1,5 @@
+﻿namespace FirstApp.Models;
+
+public record StatusResponseModel(DateTime LastChecked, string Message, string Status);
+
+public record TodoItemModel(Guid Id, string Description, string Status);
---
+++ FirstAppSolution/FirstApp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using FirstApp.Models;
+
+Console.WriteLine("Starting Up The Api");
+var builder = WebApplication.CreateBuilder(args);
+// Configuration for the API Will go here.
+
+
+
+Console.WriteLine("About to Start the Api");
+var app = builder.Build();
+// GET /sayhi
+app.MapGet("/sayhi", () =>
+{
+    Console.WriteLine("Got a request!");
+    return Results.Ok("Yep! Hello, Good To See You!");
+});
+
+app.MapGet("/status", () =>
+{
+    var response = new StatusResponseModel(DateTime.Now, "Looks Good", "Operating Normally");
+    return Results.Ok(response);
+});
+
+app.MapGet("/todo-list", () =>
+{
+    var response = new List<TodoItemModel>() {
+        new TodoItemModel(Guid.NewGuid(), "Buy Beer", "Later"),
+        new TodoItemModel(Guid.NewGuid(), "Mow Lawn", "Waiting")
+    };
+    return Results.Ok(response);
+});
+
+app.Run(); // Blocking call - this will start server and run FOREVER (or until we kill it)
+Console.WriteLine("Api is going down. Goodbye..");
