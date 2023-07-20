cf1a92df644b1024219e83b0a5b7fc6bea491165
Jeffry Gonzalez <jeff@hypertheory.com>
2023-07-20 12:30:19 -0400

    before lunch

---
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿namespace Banking.Domain;
+
+public class BankAccount
+{
+    private decimal _balance = 5000;
+    public void Deposit(decimal amountToDeposit)
+    {
+        _balance += amountToDeposit;
+    }
+
+    public decimal GetBalance()
+    {
+        return _balance; // This is obviously BS.
+    }
+}
---
+++ BankingSolution/Banking.UnitTests/BankAccounts/MakingWithdrawals.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Banking.Domain;
+
+namespace Banking.UnitTests.BankAccounts;
+
+public class MakingWithdrawals
+{
+    [Fact]
+    public void MakingAWithdrawalDecreasesTheBalance()
+    {
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToWithdraw = 1.01M;
+
+        account.Withdraw(amountToWithdraw);
+
+        Assert.Equal(openingBalance - amountToWithdraw,
+            account.GetBalance());
+    }
+}
