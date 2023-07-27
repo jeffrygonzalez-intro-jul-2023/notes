fc4a03e2d75b11c29c6ee982705cd4155f0b9d8e
Jeffry Gonzalez <jeff@hypertheory.com>
2023-07-27 15:18:33 -0400

    before break

--- EmployeeDirectorySolution/EmployeeDirectoryApi/Program.cs
+++ EmployeeDirectorySolution/EmployeeDirectoryApi/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeDirectoryApi;
+using EmployeeDirectoryApi.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// 198
+builder.Services.AddTransient<IProvideTheBusinessClock, StandardBusinessClock>();
+builder.Services.AddScoped<ILookupOnCallDevelopers, DeveloperLookup>();
+builder.Services.AddSingleton<ISystemTime, SystemTime>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,3 +27,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { } // this will add to the internal class Main so we can see it from out tests.
